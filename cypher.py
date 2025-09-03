3bb8a1404cb3fda5318e608c80062cb01504e7ed
zeez71295 <zeez71295@gmail.com>
2025-09-03 09:53:53 +0100

    message to commit

--- cypher.py
+++ cypher.py
@@ -1,36 +1,30 @@
-text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'python'
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
 
-def vigenere(message, key, direction=1):
-    key_index = 0
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    final_message = ''
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
 
-    for char in message.lower():
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+    total = sum_of_odd_digits + sum_of_even_digits
+    print(total)
+    return total % 10 == 0
 
-        # Append any non-letter character to the message
-        if not char.isalpha():
-            final_message += char
-        else:        
-            # Find the right key character to encode/decode
-            key_char = key[key_index % len(key)]
-            key_index += 1
+def main():
+    card_number = '4111-1111-4555-1141'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
 
-            # Define the offset and the encrypted/decrypted letter
-            offset = alphabet.index(key_char)
-            index = alphabet.find(char)
-            new_index = (index + offset*direction) % len(alphabet)
-            final_message += alphabet[new_index]
-    
-    return final_message
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
 
-def encrypt(message, key):
-    return vigenere(message, key)
-    
-def decrypt(message, key):
-    return vigenere(message, key, -1)
-
-print(f'\nEncrypted text: {text}')
-print(f'Key: {custom_key}')
-decryption = decrypt(text, custom_key)
-print(f'\nDecrypted text: {decryption}\n')
+main()
